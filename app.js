c248c6940d44b189502f201e297b1a47d55a7620
kyrustomris <kyrustomris@gmail.com>
2025-07-10 14:56:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
 const firebaseConfig = {
   apiKey: "AIzaSyBB-PEtapGv0S6B_Xt1A-6dTMjvO5ASrNc",
   authDomain: "osmanlicaogren-57ff0.firebaseapp.com",
   projectId: "osmanlicaogren-57ff0",
-  storageBucket: "osmanlicaogren-57ff0.firebasestorage.app",
+  storageBucket: "osmanlicaogren-57ff0.appspot.com",
   messagingSenderId: "55078200434",
   appId: "1:55078200434:web:f933fe5178daaf63210eeb",
   measurementId: "G-58MD9BL1TY"
 };
 
+
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 const analytics = getAnalytics(app);
 class OsmanlicaUygulamasi {
     constructor() {
